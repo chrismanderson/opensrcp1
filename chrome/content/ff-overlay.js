e5cf6b183d1ed449e1af78a8fe622d067a249d6c
Chris Anderson <christopher.anderson@gmail.com>
2011-09-21 00:09:48 -0400

    Read/write data to xml

---
+++ chrome/content/ff-overlay.js
@@ -0,0 +1,11 @@
+cmap1.onFirefoxLoad = function(event) {
+  document.getElementById("contentAreaContextMenu")
+          .addEventListener("popupshowing", function (e){ cmap1.showFirefoxContextMenu(e); }, false);
+};
+
+cmap1.showFirefoxContextMenu = function(event) {
+  // show or hide the menuitem based on what the context menu is on
+  document.getElementById("context-cmap1").hidden = gContextMenu.onImage;
+};
+
+window.addEventListener("load", function () { cmap1.onFirefoxLoad(); }, false);
---
+++ chrome/content/overlay.js
@@ -0,0 +1,18 @@
+var cmap1 = {
+  onLoad: function() {
+    // initialization code
+    this.initialized = true;
+    this.strings = document.getElementById("cmap1-strings");
+  },
+
+  onMenuItemCommand: function(e) {
+	window.open("chrome://cmap1/content/cmap1.xul", "newdwindow", "chrome,centerscreen,resizable");
+  },
+
+  onToolbarButtonCommand: function(e) {
+    // just reuse the function above.  you can change this, obviously!
+    cmap1.onMenuItemCommand(e);
+  }
+};
+
+window.addEventListener("load", function () { cmap1.onLoad(); }, false);
